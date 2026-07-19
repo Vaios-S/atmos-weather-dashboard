0ae25bc97505cd0540d96a66e030cdcd93124fa8
Vaios-S <vaios.sav.dev@gmail.com>
2026-07-19 12:56:05 +0300

    test: add fetch weather start reducer test

--- src/tests/weatherReducer.test.js
+++ src/tests/weatherReducer.test.js
@@ -8,4 +8,13 @@ describe("weatherReducer", () => {
 
     expect(newState.unit).toBe("imperial");
   });
+
+  test("should set loading to true when fetching weather starts", () => {
+    const loadingState = weatherReducer(initialWeatherState, {
+      type: "FETCH_WEATHER_START",
+    });
+
+    expect(loadingState.loading).toBe(true);
+    expect(loadingState.error).toBe(null);
+  });
 });
